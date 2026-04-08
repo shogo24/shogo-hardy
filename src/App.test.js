5a9a14141e36261bb920b0e07908394113356db8
Shogo <shogo.a.hardy@gmail.com>
2026-04-08 08:18:53 -0600

    Add unit test for name rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders my name on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const name = screen.getByTestId('student-name');
+  expect(name).toBeInTheDocument();
+  expect(name).toHaveTextContent('Shogo Hardy');
 });
